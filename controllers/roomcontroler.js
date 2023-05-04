153d8ddda4f93ef63b016b333304ba4a0d5f8d94
StefanRudareanu <stefanrudareanu24@gmail.com>
2023-05-04 22:04:28 +0300

    almost done delete not working now

--- controllers/roomcontroler.js
+++ controllers/roomcontroler.js
@@ -2,30 +2,26 @@ const router = require("express").Router();
 const fs = require("fs");
 const roomdb = require("../rooms.json");
 router.get("/allmeetingrooms", (req, res) => {
-  res.status(200).send(roomdb);
+  res.status(200).send(roomdb.rooms);
 });
 router.get("/meetingroom/:id", (req, res) => {
   const data = roomdb.rooms.filter((e) => {
     return e.id == req.params.id;
   });
-  res.status(400).send(data);
+  res.status(200).send(data[0]);
 });
 router.delete("/meetingroom/:id", (req, res) => {
   const data = roomdb.rooms.filter((e) => {
     return e.id != req.params.id;
   });
-  if (JSON.stringify(roomdb.rooms) === JSON.stringify(data)) {
-    fs.writeFile("rooms.json", JSON.stringify({ rooms: data }), (err) => {
-      if (err) {
-        res.status(400).send({ err: err });
-      } else {
-        console.log(err);
-        res.status(200).send("Success");
-      }
-    });
-  } else {
-    res.status(400).send("Element does not exist");
-  }
+  fs.writeFile("rooms.json", JSON.stringify({ rooms: data }), (err) => {
+    if (err) {
+      res.status(400).send({ err: err });
+    } else {
+      console.log(err);
+      res.status(200).send("Success");
+    }
+  });
 });
 router.post("/meetingroom", (req, res) => {
   const data = {
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan")("tiny");
 const roomcontroller = require("./controllers/roomcontroler");
+var cors = require("cors");
 app.use(morgan);
 app.use(express.json());
+app.use(cors());
 app.use("", roomcontroller);
 app.listen(8080, () => {
   console.log("Started on 8080");
